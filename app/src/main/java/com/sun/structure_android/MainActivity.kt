bd8452b1cf4591ee4290ced2e7d0d8ea36395d5e
daolq3012 <duykhanh.t2@gmail.com>
2022-02-15 11:30:49 +0700

    Config ktlint

--- app/src/main/java/com/sun/structure_android/MainActivity.kt
+++ app/src/main/java/com/sun/structure_android/MainActivity.kt
@@ -1,11 +1,14 @@
 package com.sun.structure_android
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        val ABDHDHJS = 1
+
         setContentView(R.layout.activity_main)
     }
 }
